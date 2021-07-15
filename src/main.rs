5069eb2b8807a4851d59d4ed5b9c3905a1efa334
Shane McDonough <shanemcdman@gmail.com>
2021-07-15 13:22:33 -0700

    Get line count to work

--- src/main.rs
+++ src/main.rs
@@ -5,23 +5,21 @@ use std::io::stdin;
 use std::path::PathBuf;
 use structopt::StructOpt;
 
-fn print_file(file_name: &PathBuf, opts: &Opt) -> Result<(), std::io::Error> {
-    let mut string = String::new();
+fn print_file(file_name: &PathBuf, opts: &Opt, line_count: &mut i32) -> Result<(), std::io::Error> {
     let mut file = File::open(file_name)?;
-    print_lines(&mut file, opts)?;
+    print_lines(&mut file, opts, line_count)?;
     Ok(())
 }
 
-fn echo_input(opt: &Opt) {
-    print_lines(&mut stdin(), &opt);
+fn echo_input(opt: &Opt, line_count: &mut i32) {
+    print_lines(&mut stdin(), &opt, line_count).unwrap();
 }
 
-fn print_lines(file: &mut dyn Read, opts: &Opt) -> Result<(), std::io::Error> {
-    let mut i = 0;
+fn print_lines(file: &mut dyn Read, opts: &Opt, line_count: &mut i32) -> Result<(), std::io::Error> {
     for line in BufReader::new(file).lines() {
         if opts.number{
-            println!("{} {}", i, line.unwrap());
-            i += 1;
+            *line_count += 1;
+            println!("{: >6}: {}", line_count, line.unwrap());
         } else {
             println!("{}", line.unwrap());
         }
@@ -41,14 +39,15 @@ struct Opt {
 
 fn main() {
     let opt = Opt::from_args();
+    let mut line_count = 0;
     match opt.files.len() {
-        0 => echo_input(&opt), // no args; repeat until ctrl-c
+        0 => echo_input(&opt, &mut line_count), // no args; repeat until ctrl-c
         _ => {
             for file in &opt.files {
                 if file.as_os_str().to_str() == Some("-") {
-                    echo_input(&opt);
+                    echo_input(&opt, &mut line_count);
                 } else {
-                    match print_file(&file, &opt) {
+                    match print_file(&file, &opt, &mut line_count) {
                         Err(e) => println!("Opt: Cannot Open {}: {}", file.display(), e),
                         _ => (),
                     }
